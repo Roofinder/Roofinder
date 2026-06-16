0da75ab43aefea25fffb1099160f1e8cbaeae88f
Roofinder <hello@roofinder.online>
2026-06-16 16:18:50 -0400

    fix: api/score-roof.js

--- api/score-roof.js
+++ api/score-roof.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -75,4 +75,4 @@ export default async function handler(req, res) {
       content.push({ type: 'text', text: `[View: ${img.label}]` });
       content.push({
         type: 'image',
-        source: { type: 'base64', media_type: 'image/jpeg', data: img.b64 
+        source: { type: 'base64', media_type: 'image/jpeg', data: img.b6
